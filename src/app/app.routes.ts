01d0b25af4ccc63f418f58e08cd0be17d38741ce
nestserka <nestserava.k@gmail.com>
2024-08-20 01:32:14 +0900

    feat(project): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
 
-export const APP_ROUTES: Routes = [];
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/home/constants/home.routes').then(
+        (m) => m.HOME_ROUTES,
+      ),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/authorization/constants/authorization.routes').then(
+        (m) => m.AUTHORIZATION_ROUTES,
+      ),
+  },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./features/orders/constants/orders.routes').then(
+        (m) => m.ORDERS_ROUTES,
+      ),
+  },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./features/profile/constants/profile.routes').then(
+        (m) => m.PROFILE_ROUTES,
+      ),
+  },
+
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./core/not-found/components/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent,
+      ),
+  },
+  { path: '**', redirectTo: 'not-found' },
+];
---
+++ src/app/features/authorization/constants/authorization.routes.ts
@@ -0,0 +1,3 @@
+import { Routes } from '@angular/router';
+
+export const AUTHORIZATION_ROUTES: Routes = [];
