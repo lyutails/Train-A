2cc30996da2eca16a16792302dc213ebecdff087
Mardon07 <mardonberdiyev10@gmail.com>
2024-08-20 23:12:04 +0500

    feat(component): create profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserProfileComponent } from './features/profile/features/components/user-profile/user-profile.component';
 
 @Component({
   selector: 'TTP-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
