859bcfa00016383a2ba0527b0ad0fc80e388c9bb
lyutails <lyusya.cool@gmail.com>
2024-08-20 03:38:34 +0300

    feat(forms): create form base inputs and buttons

--- src/app/features/authorization/features/sign-up/components/sign-up/sign-up.component.ts
+++ src/app/features/authorization/features/sign-up/components/sign-up/sign-up.component.ts
@@ -1,24 +1,48 @@
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import {
+  MatFormField,
+  MatInput,
+  MatLabel,
+  MatSuffix,
+} from '@angular/material/input';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'TTP-sign-up',
   standalone: true,
-  imports: [CommonModule, MatButton, MatInput, MatIcon, MatLabel, MatFormField],
+  imports: [
+    CommonModule,
+    MatButton,
+    MatInput,
+    MatIcon,
+    MatLabel,
+    MatFormField,
+    MatIconButton,
+    MatSuffix,
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
 })
 export class SignUpComponent {
   signUpButtonName = 'Register';
   signInButtonName = 'Sign In';
+  formTitle = 'Sign Up';
+  hide = signal(true);
 
   constructor(private router: Router) {}
 
+  togglePasswordVisibility() {
+    this.hide.set(!this.hide());
+  }
+
   navigateSignIn() {
     this.router.navigate(['/signin']);
+  }
+
+  registerUser() {
+    console.log('lalala');
   }
 }
