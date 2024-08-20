66773fba8e4397ed1f3446583e9d2d020876e59d
Mardon07 <mardonberdiyev10@gmail.com>
2024-08-20 23:12:04 +0500

    feat(component): create profile page

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { startServer } from '@planess/train-a-backend';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { startServer } from '@planess/train-a-backend';
 
-startServer()
-  .then(() => bootstrapApplication(AppComponent, appConfig))
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
