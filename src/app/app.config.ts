6d3087c5136eb42deaaf32fabfbfdbe14c54e18d
nestserka <nechatherina@gmail.com>
2024-08-21 12:38:33 +0900

    feat(storage): add local storage service for token management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,5 @@
-import {
-  ApplicationConfig,
-  provideZoneChangeDetection,
-  isDevMode,
-  importProvidersFrom,
-} from '@angular/core';
-import {
-  PreloadAllModules,
-  provideRouter,
-  withPreloading,
-} from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode, importProvidersFrom } from '@angular/core';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { APP_ROUTES } from './app.routes';
 import { provideStore } from '@ngrx/store';
@@ -17,6 +8,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideEffects } from '@ngrx/effects';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MatNativeDateModule } from '@angular/material/core';
+import { StorageModule } from './core/storage/storage.module';
+import { storageKeyPrefix } from './core/storage/tokens/local-storage-key.token';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -34,5 +27,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     importProvidersFrom(MatNativeDateModule),
     provideHttpClient(withInterceptors([])),
+    importProvidersFrom(StorageModule.forRoot({ config: { prefix: storageKeyPrefix } })),
   ],
 };
