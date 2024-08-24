17990131092c6ac8d55e2e8c887d2868ba02733f
nestserka <nestserava.k@gmail.com>
2024-08-24 15:43:10 +0900

    fix(pipe): revise pipe with comma

--- src/app/features/admin/features/stations/pipes/train-connections.pipe.ts
+++ src/app/features/admin/features/stations/pipes/train-connections.pipe.ts
@@ -6,14 +6,16 @@ import { StationInfo } from '../models/station-info';
   standalone: true,
 })
 export class TrainConnectionsPipe implements PipeTransform {
-  transform(relationIds: number[], stations: StationInfo[] | null): string[] {
+  transform(relationIds: number[], stations: StationInfo[] | null): string {
     if (!relationIds || !stations) {
-      return [];
+      return '';
     }
 
-    return relationIds.map((id) => {
-      const station = stations.find((station) => station.id === id);
-      return station ? station.city : 'Unknown';
-    });
+    return relationIds
+      .map((id) => {
+        const station = stations.find((station) => station.id === id);
+        return station ? station.city : 'Unknown';
+      })
+      .join(', ');
   }
 }
