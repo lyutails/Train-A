fc3eab37eea3b5780340aecc48579ac679766406
lyutails <lyusya.cool@gmail.com>
2024-08-25 04:40:14 +0300

    feat(component): click on lable change color

--- src/app/features/carriages/features/components/carriage-seat/carriage-seat.component.ts
+++ src/app/features/carriages/features/components/carriage-seat/carriage-seat.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'TTP-carriage-seat',
   standalone: true,
-  imports: [MatCheckboxModule, MatLabel, MatIcon],
+  imports: [MatCheckboxModule, MatLabel, MatIcon, MatInput],
   templateUrl: './carriage-seat.component.html',
   styleUrl: './carriage-seat.component.scss',
 })
 export class CarriageSeatComponent {
   @Input() seatValue = '';
   @Input() carriageNameValue!: string;
+  check = signal(false);
+
+  clickCheckbox() {
+    this.check.set(!this.check());
+  }
 }
