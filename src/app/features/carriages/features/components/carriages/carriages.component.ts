533c823820aeab844458920986bea5ba7a1ce68a
lyutails <lyusya.cool@gmail.com>
2024-08-25 19:37:04 +0300

    feat(form): add validators for input and select for creating carriage

--- src/app/features/carriages/features/components/carriages/carriages.component.ts
+++ src/app/features/carriages/features/components/carriages/carriages.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { ButtonComponent } from '../../../../../common/button/button.component';
 import { HttpClient } from '@angular/common/http';
-import { FormControl, FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CommonModule } from '@angular/common';
-import { Carriage, CarriageForm } from '../../models/carriages.model';
+import { Carriage } from '../../models/carriage.model';
 import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
 import { MatOption, MatSelect } from '@angular/material/select';
 import { Observable } from 'rxjs';
 import { CarriageRowComponent } from '../carriage-row/carriage-row.component';
+import { CarriageForm } from '../../models/carriage-form.model';
 
 export interface CarriageCreatingParams {
   value: string;
@@ -35,14 +43,12 @@ export interface CarriageCreatingParams {
   styleUrl: './carriages.component.scss',
 })
 export class CarriagesComponent implements OnInit {
-  /* public carriageForm!: FormGroup<Carriage>; */
-  public carriagesArray!: FormGroup<CarriageForm>[];
-  create = signal(false);
-  update = signal(false);
   selectedRowsValue = '';
   selectedLeftSideSeatsValue = '';
   selectedRightSideSeatsValue = '';
   carriagesData!: Carriage[];
+  /* public carriageForm!: FormGroup<Carriage>; */
+  public carriagesArray!: FormGroup<CarriageForm>[];
   retrievedCarriagesForm = new FormGroup({
     code: new FormControl(''),
     name: new FormControl(''),
@@ -50,6 +56,9 @@ export class CarriagesComponent implements OnInit {
     leftSeats: new FormControl(0),
     rightSeats: new FormControl(0),
   });
+  public createCarriageForm!: FormGroup<CarriageForm>;
+  create = signal(false);
+  update = signal(false);
 
   possibleRows: CarriageCreatingParams[] = [
     { value: 'row-2', viewValue: '2' },
@@ -72,6 +81,31 @@ export class CarriagesComponent implements OnInit {
   possibleRightSeats: CarriageCreatingParams[] = [
     { value: 'rightSeats-1', viewValue: '1' },
     { value: 'rightSeats-2', viewValue: '2' },
+    { value: 'leftSeats-3', viewValue: '3' },
+  ];
+
+  initialPostCarriagesArray = [
+    {
+      code: '',
+      name: 'carriage2F',
+      rows: 12,
+      leftSeats: 2,
+      rightSeats: 2,
+    },
+    {
+      code: '',
+      name: 'carriage8K',
+      rows: 10,
+      leftSeats: 3,
+      rightSeats: 3,
+    },
+    {
+      code: '',
+      name: 'carriageVIP',
+      rows: 6,
+      leftSeats: 1,
+      rightSeats: 1,
+    },
   ];
 
   constructor(
@@ -107,7 +141,7 @@ export class CarriagesComponent implements OnInit {
   }
 
   public postCarriage() {
-    return this.httpClient.post<Carriage>('carriage', this.testCarriagesArray[2]);
+    return this.httpClient.post<Carriage>('carriage', this.initialPostCarriagesArray[2]);
   }
 
   public updateCarriages(code: string, body: Carriage) {
@@ -120,12 +154,33 @@ export class CarriagesComponent implements OnInit {
     }); */
 
     this.create.set(!this.create());
-    return this.fb.group({
-      name: this.fb.control({ value: '', disabled: false }),
-      rows: this.fb.control({ value: 0, disabled: false }),
-      leftSeats: this.fb.array([new FormControl()]),
-      rightSeats: this.fb.array([new FormControl()]),
-    });
+
+    return (this.createCarriageForm = this.fb.group<CarriageForm>({
+      name: this.fb.control(
+        { value: '', disabled: false },
+        {
+          validators: [Validators.required, Validators.minLength(2), Validators.maxLength(20)],
+        },
+      ),
+      rows: this.fb.control(
+        { value: 0, disabled: false },
+        {
+          validators: [Validators.required],
+        },
+      ),
+      leftSeats: this.fb.control(
+        { value: 0, disabled: false },
+        {
+          validators: [Validators.required],
+        },
+      ),
+      rightSeats: this.fb.control(
+        { value: 0, disabled: false },
+        {
+          validators: [Validators.required],
+        },
+      ),
+    }));
   }
 
   /* public saveCarriage() {
@@ -135,30 +190,6 @@ export class CarriagesComponent implements OnInit {
   /* ngOnInit() {
     this.carriageForm = this.carriageFormInstance;
   } */
-
-  testCarriagesArray = [
-    {
-      code: '',
-      name: 'carriage2F',
-      rows: 12,
-      leftSeats: 2,
-      rightSeats: 2,
-    },
-    {
-      code: '',
-      name: 'carriage8K',
-      rows: 10,
-      leftSeats: 3,
-      rightSeats: 3,
-    },
-    {
-      code: '',
-      name: 'carriageVIP',
-      rows: 6,
-      leftSeats: 1,
-      rightSeats: 1,
-    },
-  ];
 
   private get carriageFormInstance() {
     return this.fb.group({
@@ -177,7 +208,7 @@ export class CarriagesComponent implements OnInit {
     return this.carriageForm.controls.rightSeats;
   } */
 
-  /*  public paintCarriage(this.testCarriagesArray[0]) {
+  /*  public paintCarriage(this.initialPostCarriagesArray[0]) {
 
   } */
 }
--- src/app/features/carriages/features/models/carriage-form.model.ts
+++ src/app/features/carriages/features/models/carriage-form.model.ts
@@ -1,13 +1,5 @@
 import { FormControl } from '@angular/forms';
 
-export interface Carriage {
-  code?: string;
-  name?: string;
-  rows: number;
-  leftSeats: number;
-  rightSeats: number;
-}
-
 export interface CarriageForm {
   code?: FormControl<string>;
   name?: FormControl<string>;
