48016464dc4f30ac75b9218c9ed81da4c3924136
lyutails <lyusya.cool@gmail.com>
2024-08-25 11:09:42 +0300

    feat(component): add catching checked event for seat checkbox

--- src/app/features/carriages/features/components/carriage-seat/carriage-seat.component.ts
+++ src/app/features/carriages/features/components/carriage-seat/carriage-seat.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, signal } from '@angular/core';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Input, signal, ViewChild } from '@angular/core';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
 import { MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
@@ -7,20 +8,23 @@ import { MatInput } from '@angular/material/input';
 @Component({
   selector: 'TTP-carriage-seat',
   standalone: true,
-  imports: [MatCheckboxModule, MatLabel, MatIcon, MatInput],
+  imports: [MatCheckboxModule, MatLabel, MatIcon, MatInput, CommonModule],
   templateUrl: './carriage-seat.component.html',
   styleUrl: './carriage-seat.component.scss',
 })
 export class CarriageSeatComponent {
   @Input() seatValue = '';
   @Input() carriageNameValue!: string;
+  @ViewChild('seatCheckbox') seatCheckbox!: ElementRef;
   check = signal(false);
+  // isChecked!: boolean;
 
   clickCheckbox() {
     this.check.set(!this.check());
   }
 
-  clicked() {
-    console.log('checked');
+  inspectCheckboxValue(event: MatCheckboxChange): void {
+    console.log(event.checked);
+    console.log(this.seatCheckbox);
   }
 }
