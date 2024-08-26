2edf6d64b21df02b93f4a7c27e929dbb6966b180
lyutails <lyusya.cool@gmail.com>
2024-08-26 17:06:21 +0300

    feat(component): correct placement of seats

--- src/app/features/carriages/features/components/carriage-row/carriage-row.component.ts
+++ src/app/features/carriages/features/components/carriage-row/carriage-row.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, computed, Input, OnChanges, OnInit, signal } from '@angular/core';
 import { CarriageSeatComponent } from '../carriage-seat/carriage-seat.component';
 
 @Component({
@@ -8,15 +8,29 @@ import { CarriageSeatComponent } from '../carriage-seat/carriage-seat.component'
   templateUrl: './carriage-row.component.html',
   styleUrl: './carriage-row.component.scss',
 })
-export class CarriageRowComponent implements OnInit {
+export class CarriageRowComponent implements OnInit, OnChanges {
   @Input() rowNumber = 0;
   @Input() leftSeatCount = 0;
   @Input() rightSeatCount = 0;
   @Input() carriageName!: string;
   seatsInRow!: number[];
   seatsInRowNumber!: number;
+  leftSeatSignal = signal(0);
+  rightSeatSignal = signal(0);
 
   ngOnInit(): void {
     this.seatsInRowNumber = this.leftSeatCount + this.rightSeatCount;
+    this.leftSeatSignal.set(this.leftSeatCount);
+    this.rightSeatSignal.set(this.rightSeatCount);
   }
+
+  ngOnChanges(): void {
+    this.seatsInRowNumber = this.leftSeatCount + this.rightSeatCount;
+    this.leftSeatSignal.set(this.leftSeatCount);
+    this.rightSeatSignal.set(this.rightSeatCount);
+  }
+
+  public totalSeats = computed(() => {
+    return this.leftSeatSignal() + this.rightSeatSignal();
+  });
 }
