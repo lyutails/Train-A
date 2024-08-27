8ad509492b318bfaf54273ca13d349ace806b3ba
lyutails <lyusya.cool@gmail.com>
2024-08-27 14:02:33 +0300

    feat(component): validate required from and to

--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepicker, MatDatepickerModule, MatDatepickerToggle } from '@angular/material/datepicker';
@@ -45,20 +45,48 @@ export class HomeComponent implements OnInit {
 
   private get searchFormInstance(): FormGroup<SearchForm> {
     return this.fb.group<SearchForm>({
-      from: this.fb.control({ value: '', disabled: false }),
+      from: this.fb.control(
+        { value: '', disabled: false },
+        {
+          validators: [Validators.required],
+        },
+      ),
       to: this.fb.control(
         { value: '', disabled: false },
         {
-          validators: [Validators.required, Validators.minLength(2), Validators.maxLength(20)],
+          validators: [Validators.required],
         },
       ),
       date: this.fb.control(
         { value: '', disabled: false },
         {
-          validators: [Validators.required, Validators.pattern('^([1-9]|1[0-8])$')],
+          validators: [
+            Validators.required,
+            Validators.pattern('^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)dd$'),
+          ],
         },
       ),
     });
+  }
+
+  public get searchFromFormControl(): FormControl<string> {
+    return this.searchForm.controls.from;
+  }
+
+  public get searchToFormControl(): FormControl<string> {
+    return this.searchForm.controls.to;
+  }
+
+  public get searchDateFormControl(): FormControl<string> {
+    return this.searchForm.controls.date;
+  }
+
+  public searchCarriages() {
+    // api call here
+  }
+
+  public searchTrips() {
+    // api call here
   }
 
   onSubmit() {
