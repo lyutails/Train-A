c5fc0086234f0e2b18585fef56444fd29a9cb143
lyutails <lyusya.cool@gmail.com>
2024-08-27 22:35:40 +0300

    feat(component): implement edit save button with changing icon

--- src/app/features/admin/features/rides/components/rides.component.ts
+++ src/app/features/admin/features/rides/components/rides.component.ts
@@ -8,14 +8,7 @@ import { AsyncPipe, CommonModule, DatePipe, NgFor, NgIf } from '@angular/common'
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RideRoute } from '../models/route';
 import { getPricesByValue } from '../helpers/get-prices-by-value';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  NonNullableFormBuilder,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatButtonModule, MatIconButton } from '@angular/material/button';
@@ -78,7 +71,6 @@ export class RidesComponent implements OnInit, OnDestroy {
   ) {}
 
   public ngOnInit(): void {
-    this.priceForm = this.priceFormInstance;
     this.activatedRoute.data
       .pipe(
         map((data: { route?: RideRoute }) => data.route),
@@ -94,7 +86,6 @@ export class RidesComponent implements OnInit, OnDestroy {
           console.error('Failed to load ride route data', err);
         },
       });
-    this.priceForm.controls['price'].disable();
   }
 
   public ngOnDestroy(): void {
@@ -104,34 +95,5 @@ export class RidesComponent implements OnInit, OnDestroy {
 
   public returnToPreviousRoute(): void {
     this.router.navigate(['..'], { relativeTo: this.activatedRoute });
-  }
-
-  private get priceFormInstance(): FormGroup<PriceForm> {
-    return this.fb.group<PriceForm>({
-      price: this.fb.control(
-        {
-          value: 0,
-          disabled: false,
-        },
-        [Validators.required, Validators.pattern('^[1-9][0-9]*$')],
-      ),
-    });
-  }
-
-  public get priceFormControl(): FormControl<number> {
-    return this.priceForm.controls.price;
-  }
-
-  editPrice() {
-    this.priceForm.controls['price'].enable();
-    this.editSavePrice.set(false);
-  }
-
-  savePrice() {
-    if (this.priceForm?.get('price')?.valid) {
-      this.price.value = this.priceForm.controls.price.value;
-      this.priceForm.controls['price'].disable();
-    }
-    this.editSavePrice.set(true);
   }
 }
