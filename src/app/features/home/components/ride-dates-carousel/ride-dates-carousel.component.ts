307dd9dcf5f49834bdf714fe7f8d78983e95e0d6
lyutails <lyusya.cool@gmail.com>
2024-08-29 20:37:15 +0300

    feat(component): change filter slider item colour on click

--- src/app/features/home/components/ride-dates-carousel/ride-dates-carousel.component.ts
+++ src/app/features/home/components/ride-dates-carousel/ride-dates-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { TripDates } from '../home/home.component';
 
 @Component({
@@ -10,4 +10,9 @@ import { TripDates } from '../home/home.component';
 })
 export class RideDatesCarouselComponent {
   @Input() dateItem!: TripDates;
+  public selectDateFilterItem = signal(false);
+
+  setColour() {
+    this.selectDateFilterItem.set(true);
+  }
 }
