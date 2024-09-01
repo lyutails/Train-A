a11052dc32a5477366eba400814be123a826da8e
lyutails <lyusya.cool@gmail.com>
2024-09-01 17:20:41 +0300

    feat(component): add back button to redirect to home page

--- src/app/features/trip-details/components/trip-details/trip-details.component.ts
+++ src/app/features/trip-details/components/trip-details/trip-details.component.ts
@@ -1,10 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonComponent } from '../../../../common/button/button.component';
 
 @Component({
   selector: 'TTP-trip-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './trip-details.component.html',
   styleUrl: './trip-details.component.scss',
 })
-export class TripDetailsComponent {}
+export class TripDetailsComponent {
+  constructor(private readonly router: Router) {}
+
+  redirectToHomePage() {
+    this.router.navigate(['/']);
+  }
+}
