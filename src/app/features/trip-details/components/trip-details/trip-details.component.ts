c4b751baff1466f7ef19e2e0e1a7774b6c83dfa0
lyutails <lyusya.cool@gmail.com>
2024-09-01 18:04:06 +0300

    feat(component): add description for seats colour coding

--- src/app/features/trip-details/components/trip-details/trip-details.component.ts
+++ src/app/features/trip-details/components/trip-details/trip-details.component.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonComponent } from '../../../../common/button/button.component';
+import { MatIcon } from '@angular/material/icon';
+import { MatLabel } from '@angular/material/form-field';
+import { MatCheckbox } from '@angular/material/checkbox';
 
 @Component({
   selector: 'TTP-trip-details',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, MatIcon, MatLabel, MatCheckbox],
   templateUrl: './trip-details.component.html',
   styleUrl: './trip-details.component.scss',
 })
