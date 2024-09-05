50e8ae21cc34430c256a8225a6d4083eac1b416a
nestserka <nestserava.k@gmail.com>
2024-09-05 12:30:09 +0900

    feat(service): calculate a total of available places

--- src/app/features/home/components/home-ride/home-ride.component.ts
+++ src/app/features/home/components/home-ride/home-ride.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, model, signal } from '@angular/core';
+import { Component, inject, Input, model, OnDestroy, signal } from '@angular/core';
 import { ButtonComponent } from '../../../../common/button/button.component';
 import { MatIcon } from '@angular/material/icon';
 import { CommonModule, DatePipe, KeyValuePipe } from '@angular/common';
@@ -7,6 +7,10 @@ import { RouteModalComponent } from '../route-modal/route-modal.component';
 import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
 import { SearchRideResult } from '../../models/search-ride-result';
 import { Router } from '@angular/router';
+import { CarriagesService } from '../../../../repositories/carriages/services/carriages.service';
+import { map, Subject, takeUntil } from 'rxjs';
+import { CarriagesWithPrice } from '../../models/carriages-with-prices';
+import { calculateAvailableSeatsByCarriage } from '../home/helpers/calculate-availabe-seats';
 
 @Component({
   selector: 'TTP-home-ride',
@@ -24,14 +28,48 @@ import { Router } from '@angular/router';
   templateUrl: './home-ride.component.html',
   styleUrl: './home-ride.component.scss',
 })
-export class HomeRideComponent {
+export class HomeRideComponent implements OnDestroy {
   @Input() tripInfo!: SearchRideResult;
   public searchCarriages = signal(false);
   public dialog = inject(MatDialog);
   public popupRoute = signal('');
   public routeValue = model('');
+  public carriagesWithPrices: CarriagesWithPrice[] = [];
+  private readonly destroy$$ = new Subject<void>();
 
-  constructor(private readonly router: Router) {}
+  constructor(
+    private readonly router: Router,
+    private readonly carriagesService: CarriagesService,
+  ) {
+    this.carriagesService
+      .getCarriages()
+      .pipe(
+        map((carriages) => {
+          const availableSeatsData = calculateAvailableSeatsByCarriage(carriages, this.tripInfo.occupiedSeats);
+          const availableSeatsMap = new Map<string, number>(
+            availableSeatsData.map((item) => [item.carriageCode, item.availableSeats]),
+          );
+
+          const carriagesWithPrices = carriages.map((carriage) => {
+            const price = carriage.code ? this.tripInfo.price[carriage.code] : undefined;
+            const availableSeats =
+              availableSeatsMap.get(carriage.code || '') || carriage.rows * (carriage.leftSeats + carriage.rightSeats);
+
+            return {
+              ...carriage,
+              price,
+              availableSeats,
+            };
+          });
+
+          return carriagesWithPrices;
+        }),
+        takeUntil(this.destroy$$),
+      )
+      .subscribe((carriagesWithPrices) => {
+        this.carriagesWithPrices = carriagesWithPrices;
+      });
+  }
 
   public pickTripDetails() {
     this.router.navigate(['/trip', this.tripInfo.rideId], {
@@ -54,5 +92,10 @@ export class HomeRideComponent {
     dialogRef.afterClosed().subscribe(() => {
       return;
     });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$$.next();
+    this.destroy$$.complete();
   }
 }
--- src/app/features/home/components/home/helpers/calculate-price-by-segment.ts
+++ src/app/features/home/components/home/helpers/calculate-price-by-segment.ts
@@ -1,12 +1,13 @@
 import { TripSchedule } from '../../../../../repositories/home/models/trip-schedule.model';
 
-export const calculateTotalPricesByCarriage = (
+export const calculateTotalPricesAndSeatByCarriage = (
   schedule: TripSchedule,
   departureIndex: number,
   arrivalIndex: number,
-): Promise<Record<string, number>> => {
+): Promise<{ prices: Record<string, number>; takenSeats: number[] }> => {
   return new Promise((resolve, reject) => {
     const totalPricesByCarriage: Record<string, number> = {};
+    const takeSeats: Set<number> = new Set<number>();
 
     if (departureIndex < 0 || arrivalIndex >= schedule.segments.length || departureIndex > arrivalIndex) {
       return reject(new Error('Invalid departure or arrival index'));
@@ -14,15 +15,19 @@ export const calculateTotalPricesByCarriage = (
 
     for (let i = departureIndex; i <= arrivalIndex; i++) {
       const segment = schedule.segments[i];
-
       for (const [carriageType, price] of Object.entries(segment.price)) {
         if (!totalPricesByCarriage[carriageType]) {
           totalPricesByCarriage[carriageType] = 0;
         }
         totalPricesByCarriage[carriageType] += price;
       }
+
+      segment.occupiedSeats.forEach((seat) => takeSeats.add(seat));
     }
 
-    resolve(totalPricesByCarriage);
+    resolve({
+      prices: totalPricesByCarriage,
+      takenSeats: Array.from(takeSeats),
+    });
   });
 };
