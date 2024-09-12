ea12e807f8540b4547dd1286e03684110c480c1e
lyutails <lyusya.cool@gmail.com>
2024-09-12 18:46:57 +0300

    feat(ticket): add trip duration to ticket

--- src/app/features/orders/components/ticket/ticket.component.ts
+++ src/app/features/orders/components/ticket/ticket.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ButtonComponent } from '../../../../common/button/button.component';
 import { OrdersService } from '../../services/orders.service';
 import { OrderParameters } from '../../models/orders.model';
@@ -11,7 +11,7 @@ import { OrderParameters } from '../../models/orders.model';
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.scss',
 })
-export class TicketComponent {
+export class TicketComponent implements OnInit {
   @Input() stationStart!: number;
   @Input() stationEnd!: number;
   @Input() routeId!: number;
@@ -22,8 +22,17 @@ export class TicketComponent {
   @Input() status!: string;
   @Input() id!: number;
   public listOfOrders!: OrderParameters[];
+  public tripDuration!: number;
+  public hours!: number;
+  public minutes!: number;
 
   constructor(private ordersService: OrdersService) {}
+
+  ngOnInit(): void {
+    this.tripDuration = (new Date(this.arrivalTime).getTime() - new Date(this.departureTime).getTime()) / 60000;
+    this.minutes = this.tripDuration % 60;
+    this.hours = (this.tripDuration - this.minutes) / 60;
+  }
 
   public cancelOrder(id: number) {
     this.ordersService.deleteOrder(id).subscribe((data) => console.log(data));
